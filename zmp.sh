3e9acd3aecf423de87d1d19906fe7ca4431f3d91
infernalcreative <43042496+infernalcreative@users.noreply.github.com>
2020-09-16 08:35:00 +0000

    Add files via upload

---
+++ zmp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://github.com/infernalcreative/zmp/raw/master/zmp-linux-amd64
+mkdir /zmp
+cp zmp-linux-amd64 /zmp
+chmod +x zmp-linux-amd64
+chmod -R 744 /zmp
+wget https://raw.githubusercontent.com/infernalcreative/zmp/master/zmp.service -o /etc/systemd/system/zmp.service
+echo "modify /etc/systemd/system/zmp.service and run systemctl start zmp"
