97db439b6fc4be410ce01a3caa97fd1c879ae522
infernalcreative <43042496+infernalcreative@users.noreply.github.com>
2020-10-07 13:41:57 +0300

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,9 @@
+apt update
+apt install wget
+mkdir ~/.termux
+mkdir ~./termux/boot
+cd ~/.termux/boot
+wget https://github.com/infernalcreative/zmp/raw/master/zmp-android-arm64
+wget https://raw.githubusercontent.com/infernalcreative/zmp/master/termuxzmp
+chmod +x zmp-android-arm64
+chmod +x termuxzmp
