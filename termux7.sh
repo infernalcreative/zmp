782036cae54390cf2c52dc35498e2996cb8488d8
infernalcreative <43042496+infernalcreative@users.noreply.github.com>
2020-10-07 14:17:08 +0300

    Create termux7.sh

---
+++ termux7.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+mkdir -p ~/.termux/boot
+cd ~/.termux/boot
+wget https://github.com/infernalcreative/zmp/raw/master/zmp-android-arm71
+wget https://raw.githubusercontent.com/infernalcreative/zmp/master/termuxzmp7
+chmod +x zmp-android-arm71
+chmod +x termuxzmp7
